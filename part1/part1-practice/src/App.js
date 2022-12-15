89d91853681179fb928585bfcd736a8a8b3f5da0
henribrit <henriquezbrt@gmail.com>
2022-12-15 20:36:21 -0300

    Practicing along with the text.

---
+++ part1/part1-practice/src/App.js
@@ -0,0 +1,22 @@
+const App = () =>{
+  console.log ("Hello from component!")
+
+  const now = new Date()
+
+  return (
+    <div>
+      <h1> Hello World </h1>
+      <Hello name = "Henirq"/>
+      <p> It is now {now.toString()} </p>
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  return (<>
+    <p> Hello, {props.name} </p>
+    <div> Test </div>
+  </>)
+}
+
+export default App
